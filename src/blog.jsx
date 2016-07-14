79af6a2825e6e820aebc653edcc6115dc616de43
Yanan Wang <yanan.wang@citrix.com>
2016-07-14 11:10:53 +0800

    import injectTapEventPlugin from 'react-tap-event-plugin'

--- src/blog.jsx
+++ src/blog.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 const App = React.createClass({
     render: function () {
